80b2037f877d4afa54b180ec25694e73a5465f2c
Alexandr Shvets <alexandr.shvets@aras.com>
2021-07-02 13:55:47 +0300

    add actions

--- build/Deploy.ps1
+++ build/Deploy.ps1
@@ -1,14 +1,34 @@
 @"
-apiVersion: v1
-kind: Service
+apiVersion: apps/v1
+kind: Deployment
 metadata:
-  name: alex-app-service
+  name: alex-app-deployment
+  labels:
+    app: alex-app
 spec:
   selector:
-    app: alex-app
-  ports:
-    - protocol: TCP
-      port: 80
-      targetPort: http
-      name: http
+    matchLabels:
+      app: alex-app
+  template:
+    metadata:
+      labels:
+        app: alex-app
+    spec:
+      imagePullSecrets:
+      - name: regcred
+      containers:
+      - name: alexcontainerapp
+        image: alexhomeacr.azurecr.io/alexcontainerapp:v1
+        imagePullPolicy: Always
+        ports:
+        - name: http
+          containerPort: 8080
+          protocol: TCP
+        resources:
+          limits:
+            cpu: 2000m
+            memory: 512Mi
+          requests:
+            cpu: 100m
+            memory: 64Mi
 "@ | kubectl apply -f -
