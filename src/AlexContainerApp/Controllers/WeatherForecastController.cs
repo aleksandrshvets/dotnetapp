c71b9bacc63accf09da27b5702f3ec1ef395803f
Alexandr Shvets <alexandr.shvets@aras.com>
2021-07-29 23:58:33 +0300

    clean

--- src/AlexContainerApp/Controllers/WeatherForecastController.cs
+++ src/AlexContainerApp/Controllers/WeatherForecastController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 
@@ -9,24 +8,12 @@ namespace AlexContainerApp.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
             yield return new WeatherForecast
             {
-                Summary = $"{Environment.GetEnvironmentVariable("HOSTNAME") ?? "No host info"}"
+                Summary = $"POD: {Environment.GetEnvironmentVariable("HOSTNAME") ?? "No host info"}"
             };
         }
     }
